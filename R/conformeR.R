ed26da714f8f9d0b13b875a0af299a72e4c060a6
juslecl <justine.leclerc@uzh.ch>
2025-10-14 12:28:28 +0200

    Fix small things

--- R/conformeR.R
+++ R/conformeR.R
@@ -51,83 +51,83 @@ conformeR <- function(sce,
                        spacing = 0.01,
                        size_train = 0.5,
                        size_cal = .25,
-                       cores = 32) {
-    set.seed(123)
-    param <- MulticoreParam(workers = cores, RNGseed = 123)
+                       cores = 32, cutoff = .05) {
+  set.seed(123)
+  param <- MulticoreParam(workers = cores, RNGseed = 123)
 
-    # Split data
-    splits <- data_processing(sce, replicate_id, obs_condition, cell_type, size_train, size_cal)
-    properT0 <- subset_by_condition(splits$train_set, obs_condition, 0)
-    properT1 <- subset_by_condition(splits$train_set, obs_condition, 1)
-    calT0    <- subset_by_condition(splits$cal_set, obs_condition, 0)
-    calT1    <- subset_by_condition(splits$cal_set, obs_condition, 1)
-    test     <- splits$test_set
+  # Split data
+  splits <- data_processing(sce, replicate_id, obs_condition, cell_type, size_train, size_cal)
+  properT0 <- subset_by_condition(splits$train_set, obs_condition, 0)
+  properT1 <- subset_by_condition(splits$train_set, obs_condition, 1)
+  calT0    <- subset_by_condition(splits$cal_set, obs_condition, 0)
+  calT1    <- subset_by_condition(splits$cal_set, obs_condition, 1)
+  test     <- splits$test_set
 
-    groups     <- levels(splits$train_set$conf_group)
-    alphas     <- seq(spacing, 1 - spacing, spacing)
-    gene_names <- rownames(sce)
+  groups     <- levels(splits$train_set$conf_group)
+  alphas     <- seq(spacing, 1 - spacing, spacing)
+  gene_names <- rownames(sce)
 
-    # Iterate over groups
-    results <- lapply(groups, function(g) {
-      # Subset by group
-      gsets <- list(
-        T0 = properT0[properT0$conf_group == g, ],
-        T1 = properT1[properT1$conf_group == g, ],
-        C0 = calT0[calT0$conf_group == g, ],
-        C1 = calT1[calT1$conf_group == g, ],
-        Te = test[test$conf_group == g,]
-      )
-      stopifnot(ncol(gsets$T0) > 2, ncol(gsets$T1) > 2)
+  # Iterate over groups
+  results <- lapply(groups, function(g) {
+    # Subset by group
+    gsets <- list(
+      T0 = properT0[properT0$conf_group == g, ],
+      T1 = properT1[properT1$conf_group == g, ],
+      C0 = calT0[calT0$conf_group == g, ],
+      C1 = calT1[calT1$conf_group == g, ],
+      Te = test[test$conf_group == g,]
+    )
+    stopifnot(ncol(gsets$T0) > 2, ncol(gsets$T1) > 2)
 
-      idx0 <- which(gsets$Te[[obs_condition]] == 0)
-      idx1 <- which(gsets$Te[[obs_condition]] == 1)
+    idx0 <- which(gsets$Te[[obs_condition]] == 0)
+    idx1 <- which(gsets$Te[[obs_condition]] == 1)
 
-      # Loop over genes in parallel
-      gene_pvalues <- bplapply(gene_names, function(gene) {
+    # Loop over genes in parallel
+    gene_pvalues <- bplapply(gene_names, function(gene) {
 
-        # Train quantile regressions
-        qrT0 <- train_qr(gsets$T0, gene, gene_names)
-        qrT1 <- train_qr(gsets$T1, gene, gene_names)
+      # Train quantile regressions
+      qrT0 <- train_qr(gsets$T0, gene, gene_names)
+      qrT1 <- train_qr(gsets$T1, gene, gene_names)
 
-        # Propensity score
-        ps_model <- prop_score(rbind(gsets$T0, gsets$T1), gene, gene_names, obs_condition)
-        ps_cal   <- predict(ps_model, rbind(gsets$C0, gsets$C1), type = "prob") |> pull(.pred_1)
-        ps_test <- predict(ps_model, gsets$Te, type = "prob") |> pull(.pred_1)
-        w_cal    <- (1 - ps_cal) / ps_cal
-        w_test <- (1 - ps_test) / ps_test
-        wC0      <- w_cal[1:nrow(gsets$C0)]
-        wC1      <- w_cal[(nrow(gsets$C0) + 1):length(w_cal)]
+      # Propensity score
+      ps_model <- prop_score(rbind(gsets$T0, gsets$T1), gene, gene_names, obs_condition)
+      ps_cal   <- predict(ps_model, rbind(gsets$C0, gsets$C1), type = "prob") |> pull(.pred_1)
+      ps_test <- predict(ps_model, gsets$Te, type = "prob") |> pull(.pred_1)
+      w_cal    <- (1 - ps_cal) / ps_cal
+      w_test <- (1 - ps_test) / ps_test
+      wC0      <- w_cal[1:nrow(gsets$C0)]
+      wC1      <- w_cal[(nrow(gsets$C0) + 1):length(w_cal)]
 
-        # calibration scores
-        scoresT0 <- compute_cqr_scores(qrT0, gsets$C0, gene, alphas)
-        scoresT1 <- compute_cqr_scores(qrT1, gsets$C1, gene, alphas)
+      # calibration scores
+      scoresT0 <- compute_cqr_scores(qrT0, gsets$C0, gene, alphas)
+      scoresT1 <- compute_cqr_scores(qrT1, gsets$C1, gene, alphas)
 
-        int0 <- build_intervals(
-          gsets$Te[idx0, , drop = FALSE],
-          qrT1, scoresT1, wC1, w_test[idx0],
-          alphas, gene, gene_names, 0)
+      int0 <- build_intervals(
+        gsets$Te[idx0, , drop = FALSE],
+        qrT1, scoresT1, wC1, w_test[idx0],
+        alphas, gene, gene_names, 0)
 
-        int1 <- build_intervals(
-          gsets$Te[idx1, , drop = FALSE],
-          qrT0, scoresT0, wC0, w_test[idx1],
-          alphas, gene, gene_names, 0)
+      int1 <- build_intervals(
+        gsets$Te[idx1, , drop = FALSE],
+        qrT0, scoresT0, wC0, w_test[idx1],
+        alphas, gene, gene_names, 0)
 
-        int <- rbind(int0,int1) |>
-          mutate(gene=gene) |>
-          mutate(covered = sign(lower)==sign(upper)) # gene x conf_group
+      int <- rbind(int0,int1) |>
+        mutate(gene=gene) |>
+        mutate(covered = sign(lower)==sign(upper)) # gene x conf_group
 
-        tab_res <- fdr(int,g,gene,cutoff)
-        tab_res
-      }, BPPARAM = param) # closes gene loop
-      # Combine all genes for the group
-      data.table::rbindlist(gene_pvalues)[, conf_group := g]
-    }) # closes group loop
-    # Combine all groups
-    tab_res <- data.table::rbindlist(results)
+      tab_res <- fdr(int,g,gene,cutoff)
+      tab_res
+    }, BPPARAM = param) # closes gene loop
+    # Combine all genes for the group
+    data.table::rbindlist(gene_pvalues)[, conf_group := g]
+  }) # closes group loop
+  # Combine all groups
+  tab_res <- data.table::rbindlist(results)
 
-    # Here compute combined FDR
-    fdr_tab <- comb_fdr(tab_res)
-    tab_res <- tab_res |> select(-c(covered,Rg,fdr))
+  # Here compute combined FDR
+  fdr_tab <- comb_fdr(tab_res)
+  tab_res <- tab_res |> select(-c(covered,Rg,fdr))
 
-    return(list(INT=tab_res,FDR=fdr_tab))
-  }
+  return(list(INT=tab_res,FDR=fdr_tab))
+}
--- R/utils.R
+++ R/utils.R
@@ -8,7 +8,7 @@ sce_to_wide_tibble <- function(sce,obs_condition) {
   meta_df <- as.data.frame(colData(sce)) %>%
     select(conf_group, all_of(obs_condition))
 
-  expr_df <- as.data.frame(t(assay(sce)))  # transpose: rows=cells, cols=genes
+  expr_df <- as.data.frame(t(assay(sce,"logcounts")))  # transpose: rows=cells, cols=genes
   colnames(expr_df) <- rownames(sce)
 
   bind_cols(meta_df, expr_df) %>% as_tibble()
